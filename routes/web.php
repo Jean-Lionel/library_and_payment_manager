8ac0764faf7225f7d8b0b7897426ab8b249346e5
Jean Lionel <nijeanlionel@gmail.com>
2022-01-25 05:31:45 +0200

    adding blue print

--- routes/web.php
+++ routes/web.php
@@ -96,3 +96,6 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('post', App\Http\Controllers\PostController::class)->only('index', 'store');
